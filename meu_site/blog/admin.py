999c0c8563ddc6949f18152495629c8934af7ec7
Leandro de Oliveira <leandro.ult.oliveira@gmail.com>
2023-02-09 13:22:11 -0300

    Configurando URLS e listando dados na view

--- meu_site/blog/admin.py
+++ meu_site/blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'posted', 'stats')
+    list_filter = ('stats', 'created', 'posted', 'author')
+    date_hierarchy = 'posted'
+    raw_id_fields = ('author',)
+    search_fields = ('title', 'posted')
+    prepopulated_fields = {'slug': ('title',)}
+
+
 # Register your models here.
