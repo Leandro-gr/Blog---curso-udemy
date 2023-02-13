ce3f5f500f5abdb1c7e3c5aab33f5348bf13eaf0
Leandro de Oliveira <leandro.ult.oliveira@gmail.com>
2023-02-13 15:55:00 -0300

    ADD - Messages

--- meu_site/blog/views.py
+++ meu_site/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from . models import Post
@@ -14,17 +16,36 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-class BlogCreateView(CreateView):
+class BlogCreateView(SuccessMessageMixin, CreateView):
     model = Post
     template_name = 'blog/post_new.html'
     fields = '__all__'
+    success_message = "%(field)s - was create successfully"
 
-class BlogUpdateView(UpdateView):
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.title,
+        )
+
+class BlogUpdateView(SuccessMessageMixin, UpdateView):
     model = Post
     template_name = 'blog/post_edit.html'
     fields = '__all__'
+    success_message = "%(field)s - was update successfully"
+    
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.title,
+        )
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(SuccessMessageMixin, DeleteView):
     model = Post
     template_name = 'blog/post_delete.html'
     success_url = reverse_lazy('home')
+    success_message = "Deletado com sucesso"
+    
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(BlogDeleteView, self).delete(request, *args, **kwargs)
