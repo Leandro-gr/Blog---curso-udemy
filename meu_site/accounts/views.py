40d88862af2358760af092c9f7fc02c6c2126960
Leandro de Oliveira <leandro.ult.oliveira@gmail.com>
2023-02-17 11:20:23 -0300

    ADD - SignUp with Email

--- meu_site/accounts/views.py
+++ meu_site/accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
+from . forms import UserCreationFromWithEmail
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFromWithEmail
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
 
